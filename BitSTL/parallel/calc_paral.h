3d699da9cd028e38c4bdc457f61574bd4311b401
bit704 <mhn.bit@qq.com>
2024-01-23 10:44:55 +0800

    refactor: accumulate_paral

--- BitSTL/parallel/calc_paral.h
+++ BitSTL/parallel/calc_paral.h
@@ -4,16 +4,19 @@
 #ifndef CALC_PARAL_H
 #define CALC_PARAL_H
 
-#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <thread>
+#include <future>
 
 namespace bitstl
 {
     template<typename Iterator, typename T>
     struct accumulate_once
     {
-        void operator()(Iterator first, Iterator last, T& result)
+        T operator()(Iterator first, Iterator last)
         {
-            result = std::accumulate(first, last, result);
+            return std::accumulate(first, last, T());
         }
     };
 
@@ -21,7 +24,7 @@ namespace bitstl
     T accumulate_paral(Iterator first, Iterator last, T init)
     {
         const unsigned long length = std::distance(first, last);
-        if (0 == length)
+        if (!length)
             return init;
 
         unsigned long const min_per_thread = 20;
@@ -30,7 +33,7 @@ namespace bitstl
         unsigned long const num_threads = std::min(hardware_threads != 0 ? hardware_threads : 2, max_threads);
         unsigned long const true_per_thread = length / num_threads;
 
-        std::vector<T> results(num_threads);
+        std::vector<std::future<T>> futures(num_threads - 1);
         std::vector<std::thread> threads(num_threads - 1);
 
         Iterator start = first;
@@ -38,15 +41,22 @@ namespace bitstl
         {
             Iterator end = start;
             std::advance(end, true_per_thread);
-            threads[i] = std::thread(accumulate_once<Iterator, T>(), start, end, std::ref(results[i]));
+            // 使用{}初始化而不是()初始化避免编译器将其作为函数声明
+            std::packaged_task<T(Iterator, Iterator)> task{ accumulate_once<Iterator, T>() };
+            futures[i] = task.get_future();
+            threads[i] = std::thread(std::move(task), start, end);
             start = end;
         }
-        accumulate_once<Iterator, T>()(start, last, results[num_threads - 1]);
+        
+        T res = init + accumulate_once<Iterator, T>()(start, last);
 
         for (auto& entry : threads)
             entry.join();
 
-        return std::accumulate(results.begin(), results.end(), init);
+        for (auto& future : futures)
+            res += future.get();
+
+        return res;
     }
 }
 
